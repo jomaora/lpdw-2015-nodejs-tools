69bd08d35c36c1c947a5711de333f59d053ef40f
Joan Ortega <jomaora@gmail.com>
2016-02-06 12:33:57 +0100

    Adding area-test example

--- test/area-test.js
+++ test/area-test.js
@@ -1,7 +1,12 @@
-var should = require('chai').should();     // calling function
+const should = require('chai').should();     // calling function
+const geometry = require('../lib/geometry');
 
 describe('Geometry library', () => {
     describe('#triangle area', () => {
-        it('should return 10 when base is 5 and height is 4');
+        it('should return 10 when base is 5 and height is 4', () => {
+            var area = geometry.triangleArea(5, 4);
+            area.should.be.a('number');
+            area.should.equal(10);
+        });
     });
 });
