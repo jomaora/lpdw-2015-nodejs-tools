2e74a79186d273970cf6c10be83f3f0b8252366f
Joan Ortega <jomaora@gmail.com>
2016-02-06 12:43:24 +0100

    Adding second-test-degree test skeleton

---
+++ test/second-degree-test.js
@@ -0,0 +1,9 @@
+describe('Second degree library', () => {
+    it('should expose resolve function');
+
+    describe('while using resolve function', () => {
+        it('should return an array with 1 and -4 as solution for x^2 + 3x - 4 = 0');
+
+        it('should throw error for x^2 + 1 = 0');
+    });
+});
