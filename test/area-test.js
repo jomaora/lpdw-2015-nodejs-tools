7972d0bfccdbeeaa2b14091f7a0b2f86ac8577cf
Joan Ortega <jomaora@gmail.com>
2016-02-06 12:26:34 +0100

    Initializing project, adding mocha test skeleton in pending mode

---
+++ test/area-test.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();     // calling function
+
+describe('Geometry library', () => {
+    describe('#triangle area', () => {
+        it('should return 10 when base is 5 and height is 4');
+    });
+});
